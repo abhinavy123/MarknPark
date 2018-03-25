c46537fd495102ce518fced753cf2370c4ad9de9
Gialon <gialonkasha@gmail.com>
2018-03-25 13:34:28 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "heyo"
+
+print(os.getenv('IP'), os.getenv('PORT'))
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
---
+++ flaskr/flaskr/flaskr.py
@@ -0,0 +1,89 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) # load config from this file , flaskr.py
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='alphabetagama',
+    USERNAME='MarkNPark',
+    PASSWORD='losaltoshacks2018'
+))
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def get_db():
+    '''Opens a new database connection if there currently isn't one
+    for the current application context'''
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+    
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+    
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    
+@app.cli.command('initdb')
+def initdb_command():
+    '''Initializes the database'''
+    init_db()
+    print('Initialized the database.')
+    
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Closes database at end of request'''
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+        
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('Select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+    
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+    
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
